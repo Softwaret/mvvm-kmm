bee57965fb30023808f1b8d58dc344482c1e9fe3
jstarczewski <jan.a.starczewski@gmail.com>
2022-08-13 09:47:18 +0200

    Add closeable scope and type wrapper

---
+++ ios/ios/BaseViewModelAdapter.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewModelAdapter.swift
+//  ios
+//
+//  Created by Jan Starczewski on 13/08/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+
+class BaseViewModelAdapter : ObservableObject {
+    
+    
+}
---
+++ mvvm/src/iosMain/kotlin/com/softwaret/mvvvm/NativeFlowTypeWrapper.kt
@@ -0,0 +1,24 @@
+package com.softwaret.mvvvm
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+
+class NativeFlowTypeWrapper<T>(
+    private val flow: Flow<T>
+) {
+
+    fun subscribe(
+        scope: CoroutineScope,
+        onEach: (item: T) -> Unit,
+        onComplete: () -> Unit,
+        onThrow: (error: Throwable) -> Unit
+    ) = flow
+        .onEach { onEach(it) }
+        .catch { onThrow(it) }
+        .onCompletion { onComplete() }
+        .launchIn(scope)
+}
